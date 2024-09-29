5a3ab6be24d17a444647f002c747dc636a17444b
Joel Webber <jgw@pobox.com>
2024-09-29 18:55:29 -0400

    ...

--- shared.sh
+++ shared.sh
@@ -5,8 +5,9 @@ export EDITOR=nvim
 export GIT_EDITOR=nvim
 alias vim='nvim'
 
-# Make ls not suck.
+# Make ls and grep not suck.
 alias ls='ls --color -F'
+alias grep='grep --color=auto'
 
 export PS1="%F{blue}[%m]%f %F{green}%(4~|.../%3~|%~)%f > "
 
