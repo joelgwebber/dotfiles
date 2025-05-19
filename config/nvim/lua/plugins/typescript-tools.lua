e2ea0e05300835e3b4c92ea0e889f4fde7889cfd
Joel Webber <joel@fullstory.com>
2025-05-19 10:21:19 -0400

    Manual typescript-tools config, with lots of memory overhead.

--- config/nvim/lua/plugins/typescript-tools.lua
+++ config/nvim/lua/plugins/typescript-tools.lua
@@ -4,9 +4,34 @@ return {
   config = function()
     require('typescript-tools').setup {
       settings = {
-        tsserver_max_memory = 8192,
+        tsserver_max_memory = 16384,
         tsserver_file_preferences = {
           includeCompletionsForModuleExports = true,
+        },
+        tsserver_format_options = {
+          baseIndentSize = 0,
+          indentSize = 2,
+          tabSize = 2,
+          indentStyle = 'Block',
+          newLineCharacter = '\n',
+          convertTabsToSpaces = true,
+          insertSpaceAfterCommaDelimiter = true,
+          insertSpaceAfterSemicolonInForStatements = true,
+          insertSpaceBeforeAndAfterBinaryOperators = true,
+          insertSpaceAfterConstructor = false,
+          insertSpaceAfterKeywordsInControlFlowStatements = true,
+          insertSpaceAfterFunctionKeywordForAnonymousFunctions = false,
+          insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = false,
+          insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis = false,
+          insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets = false,
+          insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces = true,
+          insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces = false,
+          insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces = false,
+          insertSpaceAfterTypeAssertion = false,
+          insertSpaceBeforeFunctionParenthesis = false,
+          insertSpaceBeforeTypeAnnotation = false,
+          placeOpenBraceOnNewLineForFunctions = false,
+          placeOpenBraceOnNewLineForControlBlocks = false,
         },
       },
     }
