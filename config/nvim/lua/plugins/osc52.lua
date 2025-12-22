7f9c6bf83decb8ccca9aa62cd8a4f06c4c4b320c
Joel Webber <joel@fullstory.com>
2025-12-22 11:50:23 -0500

    Adding clipboard support over ssh.

---
+++ config/nvim/lua/plugins/osc52.lua
@@ -0,0 +1,25 @@
+return {
+  'ojroques/nvim-osc52',
+  config = function()
+    require('osc52').setup {
+      max_length = 0, -- Maximum length of selection (0 for no limit)
+      silent = false, -- Disable message on successful copy
+      trim = false, -- Trim surrounding whitespace before copy
+    }
+
+    -- Set up as default clipboard provider
+    local function copy(lines, _)
+      require('osc52').copy(table.concat(lines, '\n'))
+    end
+
+    local function paste()
+      return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
+    end
+
+    vim.g.clipboard = {
+      name = 'osc52',
+      copy = { ['+'] = copy, ['*'] = copy },
+      paste = { ['+'] = paste, ['*'] = paste },
+    }
+  end,
+}
